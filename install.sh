f31684db36012d751068b387725f56aeed8948bf
Camilo Salazar <camilo.salazar@cern.ch>
2018-05-27 11:36:14 +0200

    minor bug

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sed 's/```bash/cas1/g' README.md | sed 's/```/cas2/g' | awk '/cas1/{flag=1; next} /cas2/{flag=0} flag'
+sed 's/```bash/cas1/g' README.md | sed 's/```/cas2/g' | awk '/cas1/{flag=1; next} /cas2/{flag=0} flag' > kk.sh
 bash kk.sh
